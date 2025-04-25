7848ab6222cb45a8c2751abd25fae470540fd58e
YuzakiKokuban <heibanbaize@gmail.com>
2025-04-25 18:07:20 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ export PATH=$TOOLCHAIN/kernel-build-tools/linux-x86/bin:$PATH
 
 LLD_COMPILER_RT="-fuse-ld=lld --rtlib=compiler-rt"
 
-sysroot_flags+="--sysroot=$TOOLCHAIN/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/sysroot"
+sysroot_flags+="--sysroot=$TOOLCHAIN/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/sysroot "
 
-cflags+="-I$TOOLCHAIN/kernel-build-tools/linux-x86/include"
-ldflags+="-L $TOOLCHAIN/kernel-build-tools/linux-x86/lib64"
+cflags+="-I$TOOLCHAIN/kernel-build-tools/linux-x86/include "
+ldflags+="-L $TOOLCHAIN/kernel-build-tools/linux-x86/lib64 "
 ldflags+=${LLD_COMPILER_RT}
 
-export LD_LIBRARY_PATH="$TOOLCHAIN/kernel-build-tools/linux-x86/lib64"
+export LD_LIBRARY_PATH="$TOOLCHAIN/kernel-build-tools/linux-x86/lib64 "
 export HOSTCFLAGS="$sysroot_flags $cflags"
 export HOSTLDFLAGS="$sysroot_flags $ldflags"
 TARGET_DEFCONFIG=${1:-sun_gki_defconfig}
