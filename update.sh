d5f1fc9d4dc042cf02b8c15ed580981e34dee63b
YuzakiKokuban <heibanbaize@gmail.com>
2025-07-05 09:13:22 +0800

    升级SukiSU-Ultra

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 rm -rf ./KernelSU
-sudo curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+sudo curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-test
 git add .
 git commit -m 升级SukiSU-Ultra
 git push
