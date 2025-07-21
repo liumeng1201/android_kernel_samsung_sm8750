464b92f180bdaed5b8aa9207b6453d88ed924755
YuzakiKokuban <heibanbaize@gmail.com>
2025-07-22 07:23:29 +0800

    重构

--- net/netfilter/xt_mark.c
+++ net/netfilter/xt_mark.c
@@ -10,9 +10,8 @@
 #include <linux/module.h>
 #include <linux/skbuff.h>
 
-// Corrected include order
-#include <linux/netfilter/x_tables.h>
 #include <linux/netfilter/xt_mark.h>
+#include <linux/netfilter/x_tables.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Marc Boucher <marc@mbsi.ca>");
