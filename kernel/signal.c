6e871b415aeab42d07691f70466296f4caa61fd1
YuzakiKokuban <heibanbaize@gmail.com>
2025-10-07 05:57:50 +0800

    加入Re-Kernel

--- kernel/signal.c
+++ kernel/signal.c
@@ -57,6 +57,10 @@
 #include <asm/unistd.h>
 #include <asm/siginfo.h>
 #include <asm/cacheflush.h>
+#ifdef CONFIG_REKERNEL
+#include <uapi/asm/signal.h>
+#include <../drivers/rekernel/rekernel.h>
+#endif /* CONFIG_REKERNEL */
 #include <asm/syscall.h>	/* for syscall_get_* */
 
 #ifdef CONFIG_SAMSUNG_FREECESS
@@ -1321,6 +1325,10 @@ int do_send_sig_info(int sig, struct kernel_siginfo *info, struct task_struct *p
 {
 	unsigned long flags;
 	int ret = -ESRCH;
+#ifdef CONFIG_REKERNEL
+	if (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)
+		rekernel_report(SIGNAL, sig, task_tgid_nr(current), current, task_tgid_nr(p), p, false, NULL);
+#endif /* CONFIG_REKERNEL */
 	trace_android_vh_do_send_sig_info(sig, current, p);
 
 #ifdef CONFIG_SAMSUNG_FREECESS
